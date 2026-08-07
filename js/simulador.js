0156e09968d06347b740cc6b505921c4b05ada62
PacoDiezSa <48067615+PacoDiezSa@users.noreply.github.com>
2026-08-07 20:59:24 +0200

    Actualizaci├│n +

--- js/simulador.js
+++ js/simulador.js
@@ -422,7 +422,7 @@ function moverCursorPC(e) {
 // ==================================================
 
 const AJUSTE_MOVIL_X = 0;
-const AJUSTE_MOVIL_Y = -80;
+const AJUSTE_MOVIL_Y = 80;
 console.log("AJUSTE =", AJUSTE_MOVIL_Y);
 
 function moverCursorMovil(e) {
